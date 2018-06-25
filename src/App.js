12ddc9dfcbe53f9d266fd4891910345f7a582315
Amine <mohamed-amine.hajji@askhub.io>
2018-06-25 11:52:09 +0200

    return result

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     super(props);
     this.state = {date1 :'',
                   date2:'',
-                  date1Validation:'aaaaaaaaaa',
+                  date1Validation:'',
                   date2Validation:'',
                   daysNumber:''};
     this.handleDate1Change = this.handleDate1Change.bind(this);
@@ -21,17 +21,42 @@ class App extends Component {
   }
   handleSubmit(event) {
     event.preventDefault();
-    let date1 = this.state.date1;
-    let americanDate2 = this.state.date2;
-    let date2 = new Date(this.state.date2);
-    if(date1.match(/^[0-3]\d\/[0-1]\d\/\d{4}$/)){
-      console.log('date 1 is validate');
+    let date1 = this.state.date1.split('/');
+    let date2 = this.state.date2.split('/');
+    let date1TimeStamp = null;
+    let date2TimeStamp = null;
+    if(this.state.date1.match(/^(0[1-9]|[1-2]\d|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/) && !isNaN(new Date(date1[1]+'/'+date1[0]+'/'+date1[2]).getTime())){
+      date1TimeStamp = new Date(date1[1]+'/'+date1[0]+'/'+date1[2]).getTime();
+      this.setState({
+          date1Validation:''
+      });
+    } else {
+      this.setState({
+          date1Validation:'cette date n\'est pas valide'
+      });
     }
-    if(!this.state.date2.match(/^[0-3]\d\/[0-1]\d\/\d{4}$/)){
-      console.log('date2 is notvalid');
+    if(this.state.date2.match(/^(0[1-9]|[1-2]\d|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/) && !isNaN(new Date(date2[1]+'/'+date2[0]+'/'+date2[2]).getTime())){
+      date2TimeStamp = new Date(date2[1]+'/'+date2[0]+'/'+date2[2]).getTime();
+      this.setState({
+          date2Validation:''
+      });
+    } else {
+      this.setState({
+          date2Validation:'cette date n\'est pas valide'
+      });
     }
-    console.log('aaaaaa');
+    if(date1TimeStamp && date2TimeStamp){
+      let days =Math.trunc(Math.abs(date1TimeStamp - date2TimeStamp)/(1000*60*60*24));
+      this.setState({
+        daysNumber:'Le nombre de jours entre les 2 dates est: '+days
+      })
+    } else {
+      this.setState({
+        daysNumber:''
+      })
+    }
   }
+  // TODO: On peut changer le regex afin qu'il vérifie aussi si le mois contient 30,31 ou bien 28 jours, on peut même vérifier si l'année est bissextile (si l'utilisateur entre le mois de fevrier)
 
   render() {
     return (
@@ -49,14 +74,14 @@ class App extends Component {
             <strong>DATE 2</strong>
           </label>
           <input type="text" value={this.state.date2} onChange={this.handleDate2Change} placeholder='dd/mm/yyyy'/><br/>
-          <div>
+          <div className="required">
             {this.state.date2Validation}
           </div>
           <input type="submit" value="Search" />
         </form><br/>
-        <div>
+        <h1 className="result">
           {this.state.daysNumber}
-        </div>
+        </h1>
       </div>
     );
   }
